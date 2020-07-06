552a110c8df60e1f2977f1c5fe265ebfab4d4c6a
Coen Mathijssen <mathijssen.coen@gmail.com>
2020-07-06 18:02:02 +0200

    first build

--- vue.config.js
+++ vue.config.js
@@ -1,43 +1,24 @@
-// vue.config.js
-module.exports = {
-  module: {
-    loaders: [
+module: {
+  loaders: [
+    {
+      test: /\.vue$/,
+      loader: 'vue'
+    },
+    {
+        test: /\.s[a|c]ss$/,
+        loader: 'style!css!sass'
+    }
+  ]
+  rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue'
+       test: /\.svg$/,
+       loader: 'vue-svg-loader', // `vue-svg` for webpack 1.x
       },
-      {
-          test: /\.s[a|c]ss$/,
-          loader: 'style!css!sass'
-      }
-    ],
-    rules: [
-        {
-         test: /\.svg$/,
-         loader: 'vue-svg-loader', // `vue-svg` for webpack 1.x
-        },
-      ]
-  },
-  vue: {
-    loaders: {
-      scss: 'style!css!sass'
-    }
-  },
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.(pdf)(\?.*)?$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                name: 'files/[name].[hash:8].[ext]'
-              }
-            }
-          ]
-        }
-      ]
-    }
+    ]
+}
+
+vue: {
+  loaders: {
+    scss: 'style!css!sass'
   }
 }
